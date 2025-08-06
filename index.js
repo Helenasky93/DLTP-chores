40ed7c851420bc8cb7af847ea8c733e5ad0713ac
Helena-Zarazua <helenasky1993@gmail.com>
2025-08-06 15:18:28 -0700

    fix sintax

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-{
-  "chores": [
+module.exports = {
+  chores: [
     {
-      "title": "empty kitchen trash can",
-      "due": { "weekday": 1, "hour": 10 }
+      title: "empty kitchen trash can",
+      due: { weekday: 1, hour: 10 }
     },
     {
-      "title": "put away dishes from dishwasher",
-      "due": { "weekday": 3, "hour": 20 }
+      title: "put away dishes from dishwasher",
+      due: { weekday: 3, hour: 20 }
     }
   ],
-  "roommates": [
+  roommates: [
     {
-      "name": "Alice",
-      "slackId": "U12345678"
+      name: "Alice",
+      slackId: "U12345678"
     },
     {
-      "name": "Bob",
-      "slackId": "U23456789"
+      name: "Bob",
+      slackId: "U23456789"
     },
     {
-      "name": "Carol",
-      "slackId": "U34567890"
+      name: "Carol",
+      slackId: "U34567890"
     }
   ]
-}
+};
